f0e748d8f654be781418f3f32225da3cadb4c6a1
Subhanik Purkayastha <spurkaya@node473.oscar.ccv.brown.edu>
2019-09-20 11:13:21 -0400

    Added CCV job script

---
+++ ccv_job_array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -J 
+#SBATCH -t 12:00:00
+#SBATCH --array=1-25
+
+# Use '%A' for array-job ID, '%J' for job ID and '%a' for task ID
+#SBATCH -e arrayjob-%a.err
+#SBATCH -o arrayjob-%a.out
+
+echo "Starting job $SLURM_ARRAY_TASK_ID on $HOSTNAME"
+matlab -nodisplay -r "sim_3D_model($SLURM_ARRAY_TASK_ID,3); exit"
